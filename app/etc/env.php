1337208445c5ccb22b4a15541f0f1968e14318d1
root <root@ip-172-31-36-200.us-west-2.compute.internal>
2017-05-09 14:34:34 +0000

    add app/

---
+++ app/etc/env.php
@@ -0,0 +1,59 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_ffok1a',
+  ),
+  'crypt' => 
+  array (
+    'key' => '7f3145b7d34170607149df2a45961306',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'fbdb.czs81xeopspv.us-west-2.rds.amazonaws.com',
+        'dbname' => 'magento',
+        'username' => 'root',
+        'password' => 'BBu1w101NSPm',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Sun, 16 Apr 2017 15:48:12 +0000',
+  ),
+);
